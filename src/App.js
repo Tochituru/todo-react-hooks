15a19ad37931538a69690599bb371dbd02ac8520
Tochituru <rotrupo@gmail.com>
2020-04-19 18:05:46 -0300

    Starting hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const  App = () => {
+  let [state, setState] = useState('titulo')
+  let [inputValue, setInputValue] = useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p onClick={()=> setState('titulo final') }>{state}</p>
+      <input type={'text'} vaue={inputValue} onChange={(e) =>setInputValue(e.target.value)}></input>
     </div>
   );
 }
